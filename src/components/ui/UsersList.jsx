d9952f05f7e922a099825292c5c23b3936dbd8e5
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-27 11:51:53 +0300

    Single user, states

--- src/components/ui/UsersList.jsx
+++ src/components/ui/UsersList.jsx
@@ -1,34 +1,57 @@
 import React, { useEffect, useState } from 'react';
 
-const url = 'https://dummyjson.com/users';
+let url = 'https://dummyjson.com/users';
+// url = 'json.com/users';
+
+function SingleUser(props) {
+  return (
+    <li>
+      {props.firstName} {props.lastName}
+    </li>
+  );
+}
 
 function UsersList() {
   const [usersArr, setUsersArr] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [gotError, setGotError] = useState('');
+  // keletas komponento stadiju
+  // 1. Loading - siunciames - vyksta loading
+  // 2. Sekmingai parsiusta - nevyksta loading, nera klaidu
+  // 3. Nepavyko parsisiusti - nevyksta loading, yra klaidu
+
   // mes neturim vartotoju jei?
   const neturimVartotju = !usersArr.length;
 
   useEffect(() => {
     async function getUsers() {
       // fetch
+      setIsLoading(true);
       try {
         const resp = await fetch(url);
         const dataInJs = await resp.json();
-        console.log('dataInJs ===', dataInJs);
+        console.log('dataInJs ===', dataInJs.users);
         setUsersArr(dataInJs.users);
+        setIsLoading(false);
       } catch (error) {
         console.warn('error', error);
+        setIsLoading(false);
+        setGotError('kazkas nutiko oj oj oj');
       }
     }
     getUsers();
   }, []);
 
+  const showNoUsers = neturimVartotju && !isLoading && !gotError;
   return (
     <div>
-      <h2>UsersList</h2>
-      {neturimVartotju && <h3>Siuo metu vartotoju nera</h3>}
+      <h2>UsersList {isLoading.toString()}</h2>
+      {isLoading && <h3>Loading...</h3>}
+      {showNoUsers && <h3>Siuo metu vartotoju nera</h3>}
+      {gotError && !isLoading && <h3>Ivyko klaida: {gotError}</h3>}
       <ul>
         {usersArr.map((uObj) => (
-          <li>users</li>
+          <SingleUser key={uObj.id} lastName={uObj.lastName} firstName={uObj.firstName} />
         ))}
       </ul>
     </div>
