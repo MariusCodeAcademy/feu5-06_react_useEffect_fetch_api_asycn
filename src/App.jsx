c527ddf0708bc9e7002d315391234b13eeb18464
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-27 09:28:12 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './styles/reset.css';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>06_react_useEffect_fetch_api_asycn</h1>
+    </div>
+  );
+}
+export default App;
