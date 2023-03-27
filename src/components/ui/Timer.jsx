313c124e42ca4405ac5b23253e8014e3820eca3a
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-27 09:51:45 +0300

    useEffect

--- src/components/ui/Timer.jsx
+++ src/components/ui/Timer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './card/Card';
 
 function Timer() {
@@ -6,13 +6,20 @@ function Timer() {
   const [timerValue, setTimerValue] = useState(0);
   // let timerValue = 0;
 
-  setTimeout(() => {
-    setTimerValue(timerValue + 3);
-    // console.log('timerValue', timerValue);
-  }, 3000);
+  // useEffect(() => {}, [priklausomybiu masyvas])
+  // useEffect(() => {}, []) - toks efektas pasileidzia
+  // tik viena karta susikurus komponentui
+
+  useEffect(() => {
+    console.log('useEffect ivyko');
+    setTimeout(() => {
+      setTimerValue(timerValue + 3);
+      // console.log('timerValue', timerValue);
+    }, 3000);
+  }, []);
 
   // setTimerValue(timerValue + 3);
-
+  console.log('just Before jsx mounted');
   return (
     <Card>
       <h2>Timer</h2>
