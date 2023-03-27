9a8d67a5f0df28a56e90408aeb31a1a244b86ce2
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-27 09:55:25 +0300

    counter uzd

---
+++ src/components/ui/Counter.jsx
@@ -0,0 +1,9 @@
+// susikuriam komponenta
+// su setTimeout pakeisti title po 3sek
+// pakeista.
+
+// sukurti state, ir mygtuka kurio paspaudimu didinsim state
+// state atvizduosim jsx
+// state tadas butu zalias jei reiksme daugiau uz 5
+// state tadas butu melynas jei reiksme daugiau uz 7
+// state tadas butu raudonas jei reiksme daugiau uz 10
