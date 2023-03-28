cd989960a91afc7b95138607a750d0e7da61e9c2
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-28 09:18:59 +0300

    cleanup fn

--- src/components/posts/SinglePost.jsx
+++ src/components/posts/SinglePost.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Card from '../ui/card/Card';
 
 function SinglePost({ item, onDelete }) {
+  useEffect(() => {
+    return () => {
+      // kodas vyksta pries sunaikinant componenta
+      // componentWillUnmount()
+      console.log('deleted', item.id);
+    };
+  }, []);
+
   return (
     <Card full>
       <h3>{item.title}</h3>
